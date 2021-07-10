aeed17ae2a8eb6fe197488faad6f5f9d183e0489
Karunesh Tripathi <karunesh.tripathi2019@vitstudent.ac.in>
2021-07-10 16:00:34 +0530

    Register backend api configured

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -5,18 +5,18 @@ import "./Login.css";
 import { Link } from "react-router-dom";
 import { useHistory } from 'react-router-dom';
 export default function Login() {
-  const [id, setId] = useState("");
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
 
   function validateForm() {
-    return id.length>0 && name.length > 0 && password.length > 0;
+    return email.length>0 && name.length > 0 && password.length > 0;
   }
 
   function handleSubmit(event) {
     event.preventDefault();
     console.log("in")
-    fetch('/file/reg',{method:'POST',body:JSON.stringify({id:id,name:name,password:password}),headers: {
+    fetch('/teachers/Register',{method:'POST',body:JSON.stringify({name:name,email:email,password:password}),headers: {
         'Accept': 'application/json','Content-Type': 'application/json'}}).then((response)=>response.json()).then((data)=>{
           console.log(data.msg);
           alert(data.msg)
@@ -32,21 +32,21 @@ export default function Login() {
     <div className="Login">
       <Form onSubmit={handleSubmit}>
         <Form.Group size="lg" controlId="id">
-          <Form.Label>Project ID</Form.Label>
-          <Form.Control
-            autoFocus
-            type="text"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group size="lg" controlId="name">
-          <Form.Label>Project Name</Form.Label>
+          <Form.Label>Name</Form.Label>
           <Form.Control
             autoFocus
             type="text"
             value={name}
             onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+        <Form.Group size="lg" controlId="name">
+          <Form.Label>Email ID</Form.Label>
+          <Form.Control
+            autoFocus
+            type="text"
+            value={email}
+            onChange={(e) =>  setEmail(e.target.value)}
           />
         </Form.Group>
         <Form.Group size="lg" controlId="password">
