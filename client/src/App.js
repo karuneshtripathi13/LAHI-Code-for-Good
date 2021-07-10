81488d351019f64628bbf738a037bb7de6b4c363
Karunesh Tripathi <karunesh.tripathi2019@vitstudent.ac.in>
2021-07-10 15:50:24 +0530

    Register Import Error removed

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import Login from "./components/Login/Login";
-import SignUp from "./components/Login/signup.component";
+import SignUp from "./components/Register/Register";
 
 function App() {
   return (<Router>
