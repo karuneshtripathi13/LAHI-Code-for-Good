534fffffe34089da8869643cbfc1a83a77f4ca10
Abhiramkh <abhiramsrivathsa.is19@rvce.edu.in>
2021-07-10 22:53:58 +0530

    .

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,16 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import './Navbar.css'
-const Navbar = () => {
-    const logout=()=>{
-        localStorage.removeItem('teacher_id')
-    }
-    return (
-        <div className="Logout">
-            <h1 className="NGOheading">LAHI</h1>
-            <Link to='/' className="logoutbtn" onClick={logout}>Logout &nbsp;<i className="fa fa-sign-out"></i></Link>
-        </div>
-    )
-}
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
 
-export default Navbar
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/about' activeStyle>
+			About
+		</NavLink>
+		<NavLink to='/events' activeStyle>
+			Events
+		</NavLink>
+		<NavLink to='/annual' activeStyle>
+			Annual Report
+		</NavLink>
+		<NavLink to='/team' activeStyle>
+			Teams
+		</NavLink>
+		<NavLink to='/blogs' activeStyle>
+			Blogs
+		</NavLink>
+		<NavLink to='/sign-up' activeStyle>
+			Sign Up
+		</NavLink>
+		{/* Second Nav */}
+		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+		</NavMenu>
+		<NavBtn>
+		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
+		</NavBtn>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
