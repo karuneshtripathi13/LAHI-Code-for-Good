406aebd84b09d200c42e7d56e758cd1c56dfea21
Karunesh Tripathi <karunesh.tripathi2019@vitstudent.ac.in>
2021-07-11 06:33:51 +0530

    Master

--- client/src/components/Attendance_Handling/Attendance.js
+++ client/src/components/Attendance_Handling/Attendance.js
@@ -3,35 +3,35 @@ import './Attendence.css'
 const Attendance = () => {
     const [count, setCount] = useState(0)
     const [studentnames, setStudentnames] = useState([])
+    var arr=[]
     useEffect(() => {
-        const requestOptions = {
-            method: 'GET',
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json",
-              },
-        };
         var path='http://localhost:4000/classroom/getAttendance/'+localStorage.getItem('id')
         fetch(path)
         .then(response => response.json())
         .then(data => {
+            console.log(data)
             if(data.students.length===0)
             alert("No attendence is available")
             else
             {
             setCount(data.count)
             setStudentnames(data.students)
+            console.log(studentnames)
+            console.log(count)
+            arr=data.students
+            console.log(arr)
+            console.log(data.students)
             }
         });
        
     }, [])
     return (
-        <div className="div1">
+        <div className="attendancewrapper">
             <h1>Present Students</h1>
             {studentnames.map((student)=>
             <div >
-                <div className="studentnm">
-                <h3>{student}</h3>
+                <div >
+                <h3 className="attendencest">{student}</h3>
                 </div>
             </div>
             )}
